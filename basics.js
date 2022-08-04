923ee1ed23c8b7e110176efc2409a03e62ddaf74
Daniel3132 <danielcorrea3131@gmail.com>
2022-08-04 09:58:51 -0500

    decoradores

--- basics.js
+++ basics.js
@@ -1,4 +1,11 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 let saludo = 'hola';
 let numero;
 let cualquier;
@@ -22,3 +29,31 @@ let arregloNumeros;
 arregloNumeros = [1, 2, 3, 5];
 console.log(arregloNumeros[0]);
 /////// modulos ////////
+const persona_1 = require("./persona");
+let persona1 = new persona_1.Persona("Daniel");
+console.log(persona1.nombre);
+/*
+npm i webpack webpack-cli webpack-dev-server
+npm i typescript ts-loader
+npm init -y
+npm start
+  */
+/////// funciones flecha /////////////
+let sumaFlecha = (a, b) => {
+    return a + b;
+};
+console.log(sumaFlecha(3, 2));
+/////// decoradores /////////////
+function Saludar(target) {
+    target.prototype.saludos = function () {
+        console.log("hola desde decoradores");
+    };
+}
+let Hola = class Hola {
+    constructor() { }
+};
+Hola = __decorate([
+    Saludar
+], Hola);
+let hola1 = new Hola;
+hola1.saludos();
--- basics.ts
+++ basics.ts
@@ -1,8 +1,8 @@
 let saludo = 'hola';
-let numero:number;
-let cualquier:any;
+let numero: number;
+let cualquier: any;
 const PI = 3.24;
-function saludar(){
+function saludar() {
     numero = 7;
     console.log(saludo)
     console.log(numero)
@@ -33,21 +33,21 @@ console.log(Persona.metodoStatico()) */
 
 ///////// interface //////////////////
 
-interface Usuario{
-    nombreUsuario:string;
-    password:string;
-    confirmPass?:string; //? to mark its optional
+interface Usuario {
+    nombreUsuario: string;
+    password: string;
+    confirmPass?: string; //? to mark its optional
 }
 
-let usuario1:Usuario = {nombreUsuario: "Juan", password:"1234", confirmPass:'1234'}
+let usuario1: Usuario = { nombreUsuario: "Juan", password: "1234", confirmPass: '1234' }
 console.log(usuario1);
 console.log(usuario1.nombreUsuario);
 
-interface Abordar{
-    abordarTransporte():void;
+interface Abordar {
+    abordarTransporte(): void;
 }
-let avion:Abordar={
-    abordarTransporte: function(){
+let avion: Abordar = {
+    abordarTransporte: function () {
         console.log("abordando")
     }
 }
@@ -56,8 +56,37 @@ let avion:Abordar={
 
 let arregloNumeros: Array<number>;
 
-arregloNumeros = [1,2,3,5];
+arregloNumeros = [1, 2, 3, 5];
 
 console.log(arregloNumeros[0]);
 
 /////// modulos ////////
+
+import { Persona } from "./persona";
+let persona1 = new Persona("Daniel");
+console.log(persona1.nombre)
+
+/*
+npm i webpack webpack-cli webpack-dev-server
+npm i typescript ts-loader
+npm init -y
+npm start
+  */
+
+/////// funciones flecha /////////////
+let sumaFlecha =(a:number, b:number)=>{
+    return a + b
+}
+console.log(sumaFlecha(3,2));
+/////// decoradores /////////////
+function Saludar(target:Function):void{
+    target.prototype.saludos = function():void{
+        console.log("hola desde decoradores")
+    }
+}
+@Saludar
+class Hola{
+    constructor(){}
+}
+let hola1 = new Hola;
+hola1.saludos()
